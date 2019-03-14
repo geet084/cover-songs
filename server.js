84dcd70ee2fcb7cc16c3e0e65104183e6335a08c
Travis Gee <geet084@gmail.com>
2019-03-14 12:25:38 -0600

    Add get songs endpoint

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
   console.log(`Server is running on http://localhost:${app.get('port')}.`);
 });
+
+app.get('/api/v1/songs', (req, res) => {
+  database('songs').select()
+    .then(songs => res.status(200).json(songs))
+    .catch(error => res.status(500).json({ error }))
+})
