89ec201d9dc22a1b146db6d1f69b6684498017e9
Travis Gee <geet084@gmail.com>
2019-03-14 16:28:07 -0600

    Add error handling for incomplete information

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.get('/api/v1/songs/:id', (req, res) => {
 
 app.get('/api/v1/covers/:id', (req, res) => {
   const { id } = req.params;
+
   database('covers').where('id', id).select()
     .then(cover => {
       if (cover[0]) res.status(200).json(cover[0])
@@ -45,9 +46,18 @@ app.get('/api/v1/covers/:id', (req, res) => {
 
 app.post('/api/v1/songs', (req, res) => {
   const { song, original_artist, release_year } = req.body;
+  
+  for (let reqParam of ['song', 'original_artist', 'release_year']) {
+    if (!req.body[reqParam]) {
+      return res.status(422).send({
+        error: `Expected format: { song: <String>, original_artist: <String>, release_year: <String> }. You're missing a "${reqParam}" property.`
+      });
+    }
+  }
+
   database('songs').insert({ song, original_artist, release_year }, 'id')
     .then(song => {
-      if (song[0]) res.status(200).json(song[0])
+      if (song[0]) res.status(200).json({ id: song[0], ...req.body })
       else res.status(404).json('Selected song does not exist')
     })
     .catch(error => res.status(500).json({ error }))
@@ -55,9 +65,18 @@ app.post('/api/v1/songs', (req, res) => {
 
 app.post('/api/v1/covers', (req, res) => {
   const { song_id, cover_artist, release_year } = req.body;
+
+  for (let reqParam of ['song_id', 'cover_artist', 'release_year']) {
+    if (!req.body[reqParam]) {
+      return res.status(422).send({
+        error: `Expected format: { song_id: <String>, cover_artist: <String>, release_year: <String> }. You're missing a "${reqParam}" property.`
+      });
+    }
+  }
+
   database('covers').insert({ song_id, cover_artist, release_year }, 'id')
     .then(cover => {
-      if (cover[0]) res.status(200).json(cover[0])
+      if (cover[0]) res.status(200).json({id: cover[0], ...req.body})
       else res.status(404).json('Selected cover does not exist')
     })
     .catch(error => res.status(500).json({ error }))
@@ -65,7 +84,9 @@ app.post('/api/v1/covers', (req, res) => {
 
 app.delete('/api/v1/:db/:id', (req, res) => {
   const { db, id } = req.params;
+  const category = db === 'songs' ? "Song" : "Cover"
+  
   database(`${db}`).where('id', id).select().del()
-    .then(id => res.status(203).json(`Song ${req.params.id} removed`))
+    .then(id => res.status(203).json(`${category} ${req.params.id} removed`))
     .catch(error => res.status(500).json({ error }))
 })
