cfa59d38d9c1e8a78a1b9d6928dc5125635b6681
Travis Gee <geet084@gmail.com>
2019-03-14 12:22:22 -0600

    Add data file

---
+++ songData.js
@@ -0,0 +1,477 @@
+module.exports = [
+  {
+    "song": "Respect",
+    "original_artist": "Otis Redding",
+    "release_year": "1965",
+    "covers": [
+      {
+        "cover_artist": "Aretha Franklin",
+        "release_year": "1967"
+      },
+      {
+        "cover_artist": "Ike & Tina Turner",
+        "release_year": "1969"
+      },
+      {
+        "cover_artist": "Kelly Clarkson",
+        "release_year": "2002"
+      }
+    ]
+  },
+  {
+    "song": "Cocaine",
+    "original_artist": "J.J. Cale",
+    "release_year": "1976",
+    "covers": [
+      {
+        "cover_artist": "Eric Clapton",
+        "release_year": "1977"
+      },
+      {
+        "cover_artist": "Nazareth",
+        "release_year": "1973"
+      }
+    ]
+  },
+  {
+    "song": "Blue Suede Shoes",
+    "original_artist": "Carl Perkins",
+    "release_year": "1955",
+    "covers": [
+      {
+        "cover_artist": "Elvis Presly",
+        "release_year": "1956"
+      },
+      {
+        "cover_artist": "John Lennon",
+        "release_year": "1969"
+      }
+    ]
+  },
+  {
+    "song": "Bette Davis Eyes",
+    "original_artist": "Jackie DeShannon",
+    "release_year": "1975",
+    "covers": [
+      {
+        "cover_artist": "Kim Carnes",
+        "release_year": "1981"
+      },
+      {
+        "cover_artist": "Taylor Swift",
+        "release_year": "2011"
+      },
+      {
+        "cover_artist": "Alvin and the Chipmunks",
+        "release_year": "1982"
+      },
+      {
+        "cover_artist": "Kylie Minogue",
+        "release_year": "2014"
+      }
+    ]
+  },
+  {
+    "song": "I Will Always Love You",
+    "original_artist": "Dolly Parton",
+    "release_year": "1974",
+    "covers": [
+      {
+        "cover_artist": "Whitney Houston",
+        "release_year": "1992"
+      },
+      {
+        "cover_artist": "Kenny Rogers",
+        "release_year": "1996"
+      },
+      {
+        "cover_artist": "Michael Bolton",
+        "release_year": "2017"
+      }
+    ]
+  },
+  {
+    "song": "Hurt",
+    "original_artist": "Nine Inch Nails",
+    "release_year": "1995",
+    "covers": [
+      {
+        "cover_artist": "Johnny Cash",
+        "release_year": "2003"
+      },
+      {
+        "cover_artist": "Leona Lewis",
+        "release_year": "2011"
+      },
+      {
+        "cover_artist": "Sevendust",
+        "release_year": "2004"
+      }
+    ]
+  },
+  {
+    "song": "Jolene",
+    "original_artist": "Dolly Parton",
+    "release_year": "1974",
+    "covers": [
+      {
+        "cover_artist": "The White Stripes",
+        "release_year": "2000"
+      },
+      {
+        "cover_artist": "Reba McEntire",
+        "release_year": "1989"
+      },
+      {
+        "cover_artist": "Ellie Goulding",
+        "release_year": "2010"
+      }
+    ]
+  },
+  {
+    "song": "All Along the Watchtower",
+    "original_artist": "Bob Dylan",
+    "release_year": "1965",
+    "covers": [
+      {
+        "cover_artist": "Jimi Hendrix",
+        "release_year": "1968"
+      },
+      {
+        "cover_artist": "Neil Young",
+        "release_year": "1993"
+      },
+      {
+        "cover_artist": "Bob Dylan",
+        "release_year": "1965"
+      },
+      {
+        "cover_artist": "Dave Matthews Band",
+        "release_year": "1994"
+      },
+      {
+        "cover_artist": "U2",
+        "release_year": "1988"
+      }
+    ]
+  },
+  {
+    "song": "Stand By Me",
+    "original_artist": "Ben E. King",
+    "release_year": "1961",
+    "covers": [
+      {
+        "cover_artist": "Otis Redding",
+        "release_year": "1964"
+      },
+      {
+        "cover_artist": "Skylar Grey",
+        "release_year": "2018"
+      },
+      {
+        "cover_artist": "John Lennon",
+        "release_year": "1975"
+      },
+      {
+        "cover_artist": "The Beatles",
+        "release_year": "1981"
+      }
+    ]
+  },
+  {
+    "song": "I Heard It Through the Grapevine",
+    "original_artist": "Gladys Knight & the Pips",
+    "release_year": "1967",
+    "covers": [
+      {
+        "cover_artist": "Marvin Gaye",
+        "release_year": "1986"
+      },
+      {
+        "cover_artist": "Creedence Clearwater Revival",
+        "release_year": "1970"
+      },
+      {
+        "cover_artist": "The Temptations",
+        "release_year": "1969"
+      }
+    ]
+  },
+  {
+    "song": "Mad World",
+    "original_artist": "Tears for Fears",
+    "release_year": "1982",
+    "covers": [
+      {
+        "cover_artist": "Gary Jules",
+        "release_year": "2001"
+      },
+      {
+        "cover_artist": "Susan Boyle",
+        "release_year": "2011"
+      },
+      {
+        "cover_artist": "Adam Lambert",
+        "release_year": "2009"
+      }
+    ]
+  },
+  {
+    "song": "Over the Rainbow",
+    "original_artist": "Judy Garland",
+    "release_year": "1939",
+    "covers": [
+      {
+        "cover_artist": "Israel Kamakawiwoʻole",
+        "release_year": "1993"
+      },
+      {
+        "cover_artist": "Ariana Grande",
+        "release_year": "2017"
+      },
+      {
+        "cover_artist": "Eric Clapton",
+        "release_year": "1999"
+      },
+      {
+        "cover_artist": "Ray Charles",
+        "release_year": "1963"
+      }
+    ]
+  },
+  {
+    "song": "I Will Survive",
+    "original_artist": "Gloria Gaynor",
+    "release_year": "1978",
+    "covers": [
+      {
+        "cover_artist": "Cake",
+        "release_year": "1996"
+      },
+      {
+        "cover_artist": "Demi Lovato",
+        "release_year": "2016"
+      },
+      {
+        "cover_artist": "Diana Ross",
+        "release_year": "1993"
+      }
+    ]
+  },
+  {
+    "song": "With a Little Help From My Friends",
+    "original_artist": "The Beatles",
+    "release_year": "1967",
+    "covers": [
+      {
+        "cover_artist": "Joe Cocker",
+        "release_year": "1968"
+      },
+      {
+        "cover_artist": "Carlos Santana",
+        "release_year": "1992"
+      },
+      {
+        "cover_artist": "Bon Jovi",
+        "release_year": "1993"
+      }
+    ]
+  },
+  {
+    "song": "Tainted Love",
+    "original_artist": "Gloria Jones",
+    "release_year": "1965",
+    "covers": [
+      {
+        "cover_artist": "Marilyn Manson",
+        "release_year": "2001"
+      },
+      {
+        "cover_artist": "Soft Cell",
+        "release_year": "1981"
+      },
+      {
+        "cover_artist": "Scorpions",
+        "release_year": "2011"
+      }
+    ]
+  },
+  {
+    "song": "House of the Rising Sun",
+    "original_artist": "Clarence Ashley and Gwen Foster",
+    "release_year": "1933",
+    "covers": [
+      {
+        "cover_artist": "Five Finger Death Punch",
+        "release_year": "2013"
+      },
+      {
+        "cover_artist": "The Animals",
+        "release_year": "1964"
+      },
+      {
+        "cover_artist": "Bob Dylan",
+        "release_year": "1962"
+      },
+      {
+        "cover_artist": "The Ventures",
+        "release_year": "1964"
+      },
+      {
+        "cover_artist": "Muse",
+        "release_year": "2002"
+      },
+      {
+        "cover_artist": "Dolly Parton",
+        "release_year": "1980"
+      },
+      {
+        "cover_artist": "Toto",
+        "release_year": "2002"
+      }
+    ]
+  },
+  {
+    "song": "Twist and Shout",
+    "original_artist": "The Top Notes",
+    "release_year": "1961",
+    "covers": [
+      {
+        "cover_artist": "Salt-N-Peppa",
+        "release_year": "1988"
+      },
+      {
+        "cover_artist": "The Beatles",
+        "release_year": "1963"
+      },
+      {
+        "cover_artist": "Bruce Springsteen",
+        "release_year": "2008"
+      },
+      {
+        "cover_artist": "Michael Buble",
+        "release_year": "2009"
+      },
+      {
+        "cover_artist": "The Who",
+        "release_year": "1982"
+      },
+      {
+        "cover_artist": "Alvin and the Chipmunks",
+        "release_year": "1964"
+      }
+    ]
+  },
+  {
+    "song": "Gimme Shelter",
+    "original_artist": "The Rolling Stones",
+    "release_year": "1969",
+    "covers": [
+      {
+        "cover_artist": "Puddle of Mudd",
+        "release_year": "2011"
+      },
+      {
+        "cover_artist": "Grand Funk Railroad",
+        "release_year": "1971"
+      },
+      {
+        "cover_artist": "Stone Sour",
+        "release_year": "2015"
+      }
+    ]
+  },
+  {
+    "song": "The Sound of Silence",
+    "original_artist": "Simon & Garfunkel",
+    "release_year": "1964",
+    "covers": [
+      {
+        "cover_artist": "Disturbed",
+        "release_year": "2015"
+      },
+      {
+        "cover_artist": "Anna Kendrick",
+        "release_year": "2016"
+      }
+    ]
+  },
+  {
+    "song": "Gone Away",
+    "original_artist": "The Offspring",
+    "release_year": "1996",
+    "covers": [
+      {
+        "cover_artist": "Five Finger Death Punch",
+        "release_year": "2017"
+      }
+    ]
+  },
+  {
+    "song": "Smooth Criminal",
+    "original_artist": "Michael Jackson",
+    "release_year": "1987",
+    "covers": [
+      {
+        "cover_artist": "Alien Ant Farm",
+        "release_year": "2001"
+      }
+    ]
+  },
+  {
+    "song": "Behind Blue Eyes",
+    "original_artist": "The Who",
+    "release_year": "1965",
+    "covers": [
+      {
+        "cover_artist": "Limp Bizkit",
+        "release_year": "2003"
+      },
+      {
+        "cover_artist": "Sheryl Crow",
+        "release_year": "2001"
+      }
+    ]
+  },
+  {
+    "song": "Blank Space",
+    "original_artist": "Taylor Swift",
+    "release_year": "2014",
+    "covers": [
+      {
+        "cover_artist": "I Prevail",
+        "release_year": "2014"
+      },
+      {
+        "cover_artist": "Tyler Ward",
+        "release_year": "2014"
+      }
+    ]
+  },
+  {
+    "song": "How I Could Just Kill a Man",
+    "original_artist": "Cypress Hill",
+    "release_year": "1991",
+    "covers": [
+      {
+        "cover_artist": "Rage Against the Machine",
+        "release_year": "2000"
+      }
+    ]
+  },
+  {
+    "song": "White Rabbit",
+    "original_artist": "Jefferson Airplane",
+    "release_year": "1967",
+    "covers": [
+      {
+        "cover_artist": "Grace Potter and the Nocturnals",
+        "release_year": "2010"
+      },
+      {
+        "cover_artist": "Pink",
+        "release_year": "2017"
+      }
+    ]
+  }
+]
